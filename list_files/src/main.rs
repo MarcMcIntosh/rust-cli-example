487e16288be793ddf72e14888fb6d6bc6d53c106
Marc McIntosh <marcmcintosh1987@gmail.com>
2026-03-12 18:57:33 +0100

    task: list files.

--- list_files/src/main.rs
+++ list_files/src/main.rs
@@ -1,7 +1,8 @@
-// use std::fs;
-use std::path::Path;
+use std::fs::DirEntry;
+use std::io;
+use std::path::{Path, PathBuf};
 use clap::Parser;
-
+use clap;
 #[derive(Parser)]
 #[command(name = "list_files")]
 #[command(version = "1.0")]
@@ -13,11 +14,52 @@ struct Cli {
     recursive: bool,
 }
 
+type ListResult<T> = Result<Vec<T>, io::Error>;
+
+fn list_path_recursive(
+    path: PathBuf,
+    depth: u32,
+    dirs: Vec<PathBuf>
+) -> ListResult<PathBuf> {
+    if dirs.contains(&path) {
+        return Ok(dirs)
+    } else if path.is_file() || depth == 0 {
+        let result = [dirs, vec![path]].concat();
+        return Ok(result);
+    }
+
+    let files: Vec<PathBuf> = path
+    .read_dir()?
+    .filter_map(Result::ok)
+    .map(|dir_entry: DirEntry|  list_path_recursive(dir_entry.path(), depth - 1, dirs.clone()))
+    .collect::<ListResult<Vec<PathBuf>>>()?
+    .into_iter()
+    .flatten()
+    .collect()
+    ;
+
+    
+    return Ok(files);
+}
+
+fn list_path(path: PathBuf, recursive: bool) -> ListResult<PathBuf> {
+    let dirs = Vec::<PathBuf>::new();
+    let depth = if recursive {u32::MAX} else {1};
+    return list_path_recursive(path, depth, dirs)
+}
+
 
 fn main() {
     let cli = Cli::parse();
-    let path = Path::new(&cli.path);
-    println!("path: {:?}", path);
-    
+    let path = Path::new(&cli.path).to_path_buf();
+    let list = list_path(path, cli.recursive);
+
+    match list {
+        Err(e) => eprintln!("Error: {:?}", e),
+        Ok(files) => files.iter().for_each(|file| {
+            println!("{}", file.display())
+        })
+    }
+
 }
 
